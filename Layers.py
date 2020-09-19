b047310b3d85da96fbd12213e6bbf6e795ed319c
arthur.telles <atelles311@gmail.com>
2020-09-19 12:03:46 -0300

    update

--- Layers.py
+++ Layers.py
@@ -25,8 +25,35 @@ class DecoderLayer(nn.Module):
     def __init__(self, d_model, heads, gpt_inspired_model, dropout=0.1):
         super().__init__()
         self.gpt_inspired_model = gpt_inspired_model
-        self.norm_list = [Norm(d_model) for i in range(12)]
-        self.dropout_list = [nn.Dropout(dropout) for i in range(12)]
+        # self.norm_list = [Norm(d_model) for i in range(12)]
+        # self.dropout_list = [nn.Dropout(dropout) for i in range(12)]
+        self.norm_dict = {
+            'norm_1': Norm(d_model),
+            'norm_2': Norm(d_model),
+            'norm_3': Norm(d_model),
+            'norm_4': Norm(d_model),
+            'norm_5': Norm(d_model),
+            'norm_6': Norm(d_model),
+            'norm_7': Norm(d_model),
+            'norm_8': Norm(d_model),
+            'norm_9': Norm(d_model),
+            'norm_10': Norm(d_model),
+            'norm_11': Norm(d_model)
+        }
+
+        self.dropout_ldict = {
+            'dropout_1': nn.Dropout(dropout),
+            'dropout_2': nn.Dropout(dropout),
+            'dropout_3': nn.Dropout(dropout),
+            'dropout_4': nn.Dropout(dropout),
+            'dropout_5': nn.Dropout(dropout),
+            'dropout_6': nn.Dropout(dropout),
+            'dropout_7': nn.Dropout(dropout),
+            'dropout_8': nn.Dropout(dropout),
+            'dropout_9': nn.Dropout(dropout),
+            'dropout_10': nn.Dropout(dropout),
+            'dropout_11': nn.Dropout(dropout)
+        }
         
         self.norm_1 = Norm(d_model)
         self.norm_2 = Norm(d_model)
@@ -56,43 +83,50 @@ class DecoderLayer(nn.Module):
 
     def forward(self, x, e_outputs, src_mask, trg_mask):
         if self.gpt_inspired_model == True:
-            x2 = self.norm_list[0](x)
-            x = x + self.dropout_list[0](self.attn_1_1(x2, x2, x2, trg_mask))
-            x2 = self.norm_list[1](x)
-            x = x + self.dropout_list[1](self.attn_1_2(x2, e_outputs, e_outputs, \
+            x2 = self.norm_dict['norm_1'](x)
+            x = x + self.dropout_dict['dropout_1'](self.attn_1_1(x2, x2, x2, trg_mask))
+            x2 = self.norm_dict['norm_2'](x)
+            x = x + self.dropout_dict['dropout_2'](self.attn_1_2(x2, e_outputs, e_outputs, \
             src_mask))
-            x2 = self.norm_list[2](x)
-            x = x + self.dropout_list[2](self.ff_1(x2))
+            x2 = self.norm_dict['norm_3'](x)
+            x = x + self.dropout_dict['dropout_3'](self.ff_1(x2))
 
-            x2 = self.norm_list[3](x)
-            x = x + self.dropout_list[3](self.attn_2_1(x2, x2, x2, trg_mask))
-            x2 = self.norm_list[4](x)
-            x = x + self.dropout_list[4](self.attn_2_2(x2, e_outputs, e_outputs, \
+            x2 = self.norm_dict['norm_4'](x)
+            x = x + self.dropout_dict['dropout_4'](self.attn_2_1(x2, x2, x2, trg_mask))
+            x2 = self.norm_dict['norm_5'](x)
+            x = x + self.dropout_dict['dropout_5'](self.attn_2_2(x2, e_outputs, e_outputs, \
             src_mask))
-            x2 = self.norm_list[5](x)
-            x = x + self.dropout_list[5](self.ff_2(x2))
+            x2 = self.norm_dict['norm_6'](x)
+            x = x + self.dropout_dict['dropout_6'](self.ff_2(x2))
 
-            x2 = self.norm_list[6](x)
-            x = x + self.dropout_list[6](self.attn_3_1(x2, x2, x2, trg_mask))
-            x2 = self.norm_list[7](x)
-            x = x + self.dropout_list[7](self.attn_3_2(x2, e_outputs, e_outputs, \
+            x2 = self.norm_dict['norm_7'](x)
+            x = x + self.dropout_dict['dropout_7'](self.attn_3_1(x2, x2, x2, trg_mask))
+            x2 = self.norm_dict['norm_8'](x)
+            x = x + self.dropout_dict['dropout_8'](self.attn_3_2(x2, e_outputs, e_outputs, \
             src_mask))
-            x2 = self.norm_list[8](x)
-            x = x + self.dropout_list[8](self.ff_3(x2))
+            x2 = self.norm_dict['norm_9'](x)
+            x = x + self.dropout_dict['dropout_9'](self.ff_3(x2))
 
-            x2 = self.norm_list[9](x)
-            x = x + self.dropout_list[9](self.attn_4_1(x2, x2, x2, trg_mask))
-            x2 = self.norm_list[10](x)
-            x = x + self.dropout_list[10](self.attn_4_2(x2, e_outputs, e_outputs, \
+            x2 = self.norm_dict['norm_10'](x)
+            x = x + self.dropout_dict['dropout_10'](self.attn_4_1(x2, x2, x2, trg_mask))
+            x2 = self.norm_dict['norm_11'](x)
+            x = x + self.dropout_dict['dropout_11'](self.attn_4_2(x2, e_outputs, e_outputs, \
             src_mask))
-            x2 = self.norm_list[11](x)
-            x = x + self.dropout_list[11](self.ff_4(x2))
+            x2 = self.norm_dict['norm_12'](x)
+            x = x + self.dropout_dict['dropout_12'](self.ff_4(x2))
         else: 
-            x2 = self.norm_1(x)
-            x = x + self.dropout_1(self.attn_1(x2, x2, x2, trg_mask))
-            x2 = self.norm_2(x)
-            x = x + self.dropout_2(self.attn_2(x2, e_outputs, e_outputs, \
+            # x2 = self.norm_1(x)
+            # x = x + self.dropout_1(self.attn_1(x2, x2, x2, trg_mask))
+            # x2 = self.norm_2(x)
+            # x = x + self.dropout_2(self.attn_2(x2, e_outputs, e_outputs, \
+            # src_mask))
+            # x2 = self.norm_3(x)
+            # x = x + self.dropout_3(self.ff(x2))
+            x2 = self.norm_dict['norm_1'](x)
+            x = x + self.dropout_dict['dropout_1'](self.attn_1_1(x2, x2, x2, trg_mask))
+            x2 = self.norm_dict['norm_2'](x)
+            x = x + self.dropout_dict['dropout_2'](self.attn_1_2(x2, e_outputs, e_outputs, \
             src_mask))
-            x2 = self.norm_3(x)
-            x = x + self.dropout_3(self.ff(x2))
+            x2 = self.norm_dict['norm_3'](x)
+            x = x + self.dropout_dict['dropout_3'](self.ff_1(x2))
         return x
