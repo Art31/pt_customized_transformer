02a004ed53602294c5f2096ce1e3da14b2efa309
arthur.telles <atelles311@gmail.com>
2020-09-27 11:36:39 -0300

    update

--- Models.py
+++ Models.py
@@ -63,8 +63,8 @@ def get_model(opt, src_vocab, trg_vocab):
        
     if opt.load_weights is not None:
         print("loading pretrained weights...")
-        model = nn.DataParallel(model)
-        model.load_state_dict(torch.load(f'{opt.load_weights}/model_weights'))
+        # model = nn.DataParallel(model)
+        model.load_state_dict(torch.load(f'{opt.load_weights}/model_weights'), strict=False)
     else:
         for p in model.parameters():
             if p.dim() > 1:
