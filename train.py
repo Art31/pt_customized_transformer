c9cae20498ea25de05e7432a42228d458d4a8534
Arthur Telles <arthur.telles@nubank.com.br>
2021-03-16 21:15:27 -0300

    fix

--- train.py
+++ train.py
@@ -103,7 +103,7 @@ def train_rnn(model, iterator, optimizer, criterion, clip, opt):
             p = int(100 * (i + 1) / opt.train_len)
             avg_train_loss = epoch_loss/opt.printevery
             print("   %dm: epoch %d [%s%s]  %d%%  loss = %.3f" %\
-            ((time.time() - start)//60, epoch + 1, "".join('#'*(p//5)), "".join(' '*(20-(p//5))), p, avg_train_loss), end='\r')
+            ((time.time() - opt.start)//60, epoch + 1, "".join('#'*(p//5)), "".join(' '*(20-(p//5))), p, avg_train_loss), end='\r')
             epoch_loss = 0
         
     return epoch_loss
@@ -113,6 +113,7 @@ def train_model(model, opt): # model = NaiveModel, Transformer or Seq2Seq
     early_stopping_epochs = []
     print("training model...")
     start = time.time()
+    opt.start = start
     if opt.checkpoint > 0:
         cptime = time.time()
                  
